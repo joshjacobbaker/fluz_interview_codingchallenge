56bd8d19c0630a831efab5a11523baff77ecc195
Josh Baker <josh.jacob.baker@gmail.com>
2021-09-12 18:29:42 -0700

    added 'let' declaration for variables within function scope

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 function myfunction(input) {
   // 1 Iterate through array elements, parse the strings on commas, return array of arrays
-  sdata = input.map((i) => i.split(", "))
+  let sdata = input.map((i) => i.split(", "))
   // 2 Select 1st array element
-  sdata0 = sdata[0]
+  let sdata0 = sdata[0]
   // 3 Select 2nd array element
-  sdata1 = sdata[1]
+  let sdata1 = sdata[1]
   // 4 Combining both array elements
-  sdatacombined = sdata0.concat(sdata1)
+  let sdatacombined = sdata0.concat(sdata1)
   // 5 Filtering through combined array and returning matched value
-  sdatacombinedmatched = sdatacombined.filter((value, index, array) => {
+  let sdatacombinedmatched = sdatacombined.filter((value, index, array) => {
     return !(array.indexOf(value) === index)
   })
   // 6 Stringifying filtered array
-  output = sdatacombinedmatched.join("")
+  let output = sdatacombinedmatched.join("")
   // 7 Condition for returning value
   if (output.length > 0) {
     return output
